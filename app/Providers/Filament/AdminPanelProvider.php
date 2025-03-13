5708aab6b3051790f79f4229dc2d410d2ba3929b
Umar <arrhammzain@gmail.com>
2025-03-13 17:52:39 +0500

    update primary color in AdminPanelProvider, change default user password in UserFactory, and modify test user email in DatabaseSeeder

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -30,7 +30,7 @@ class AdminPanelProvider extends PanelProvider
             ->registration()
             ->Profile(isSimple:false)
             ->colors([
-                'primary' => Color::Green,
+                'primary' => Color::Slate,
             ])
             ->sidebarWidth('16rem')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
