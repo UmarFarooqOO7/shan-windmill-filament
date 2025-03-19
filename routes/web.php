ba65ca2196f9a2b611b6aa6f9ae575222a29c8fa
0306anees <malikanees4162@gmail.com>
2025-03-19 15:52:03 +0500

    integrate the chat in the code

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/login', function () {
+    return redirect()->route('filament.admin.auth.login'); // Redirect to Filament's login page
+})->name('login');
